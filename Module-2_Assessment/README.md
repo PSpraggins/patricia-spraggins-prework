a738b8acd63b0367faa92a0f91078a82ea6b8d15
Patricia <pdellongo@gmail.com>
2020-03-17 16:03:35 -0500

    Added functunality to game.js. Working on README doc.

---
+++ Module-2_Assessment/README.md
@@ -0,0 +1,17 @@
+# Hangman - Country Music
+
+Hangman - Country Music is an online website game. The play guesses letters to try to guess the artist name. If they get it correct before their 12 guesses are up, it alerts that they got it correct, a song by that artist pops up and it goes to the next word. If they get it wrong, it alerts them and goes to the next word. Win or lose, an image of the last artist appears. 
+
+## Technologies
+*HTML
+*CSS
+*JavaScript
+*Bootstrap 4
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,74 +1,191 @@
+//global variables
 const winsTotal = document.querySelector('#wins-total');
 const wordSpaces = document.querySelector('#word-spaces');
 const guessesRemainingId = document.querySelector('#guesses-remaining');
-const lettersGuessed = document.querySelector('#letters-guessed')
+const lettersGuessed = document.querySelector('#letters-guessed');
+const songTitle = document.querySelector('#song-title');
 const imgDiv = document.querySelector('#left-col');
+const audio = document.getElementById('audio');
 const once = {
     once: true
 };
 let num = 0;
+let word = '';
+let displayFirst = [],
+    displayLast = [];
+let firstName,
+    lastName;
 
+//game object
 const hangman = {
-    words: ["BLAKE SHELTON", "JASON ALDEAN", "TIM MCGRAW", "MIRANDA LAMBERT", "CARRIE UNDERWOOD", "KEITH URBAN", "GARTH BROOKS", "DOLLY PARTON", "WILLIE NELSON", "GEORGE STRAIT"],
+    //artist names to be guessed
+    words: [
+        "BLAKE SHELTON",
+        "JASON ALDEAN",
+        "TIM MCGRAW",
+        "MIRANDA LAMBERT",
+        "CARRIE UNDERWOOD",
+        "KEITH URBAN",
+        "GARTH BROOKS",
+        "DOLLY PARTON",
+        "WILLIE NELSON",
+        "GEORGE STRAIT"
+    ],
+    //songs to play after winning guess
+    songs: [
+        "GOD GAVE ME YOU BY BLAKE SHELTON",
+        "DIRT ROAD ANTHEM BY JASON ALDEAN",
+        "HUMBLE AND KIND BY TIM MCGRAW",
+        "THE HOUSE THAT BUILT ME BY MIRANDA LAMBERT",
+        "SOMETHING IN THE WATER BY CARRIE UNDERWOOD",
+        "COP CAR BY KEITH URBAN",
+        "FRIENDS IN LOW PLACES BY GARTH BROOKS",
+        "JOLENE BY DOLLY PARTON",
+        "ALWAYS ON MY MIND BY WILLIE NELSON",
+        "LOVE WITHOUT END, AMEN BY GEORGE STRAIT"
+    ],
     wins: 0,
     guessesRemaining: 12,
-    lettersArr: [],
-    currentWord: function(){
-        console.log("currentWord: " + this.words[num]);
-        // let num = 0;
-        imgDiv.innerHTML = `<img class= "img-fluid" src= "assets/images/${this.words[num]}.jpg" alt= "image of ${this.words[num]}">`;
+    //method to return the current word
+    //checks to see if it is the last word to display it
+    currentWord: function () {
+        if(num > this.words.length - 1){
+            imgDiv.innerHTML = `<img class= "img-fluid" src= "assets/images/${
+                this.words[this.words.length - 1]
+            }.jpg" alt= "image of ${
+                this.words[this.words.length - 1]
+            }">`;
+            songTitle.innerText = this.songs[this.words.length - 1];
+            audio.src = `assets/songs/${songTitle.innerText}.mp3`;
+            num = 0;
+            this.guessesRemaining = 12;
+            displayFirst = [];
+            displayLast = [];
+        }
         return this.words[num];
     },
-    start: function(){
-        console.log("start");
+    //start method to set up first word with hangman image
+    start: function () {
+        imgDiv.innerHTML = `<img class= "img-fluid" src= "assets/images/HANGMAN.jpg" alt= "image of hangman game">`;
         guessesRemainingId.innerText = this.guessesRemaining;
         this.wordDisplay(this.currentWord());
-        
+
     },
-    wordDisplay: function(currentWord){
+    //wordDisplay method displays the spaces for each word
+    wordDisplay: function (currentWord) {
+        displaySpaces = ' ';
         wordSpaces.innerText = '';
-        console.log("wordDisplay");
-        let word = currentWord.split(" ");
-        for(let i = 0; i < word[0].length; i++){
-            wordSpaces.innerText += ' _ ';
+        word = currentWord.split(" ");
+        firstName = word[0];
+        lastName = word[1];
+        for (let i = 0; i < firstName.length; i++) {
+            displayFirst.push(' _ ');
         }
-        wordSpaces.innerText += '\n';
-        for(let i = 0; i < word[1].length; i++){
-            wordSpaces.innerText += ' _ ';
+        for (let i = 0; i < lastName.length; i++) {
+            displayLast.push(' _ ');
+        }
+        displaySpaces = displayFirst.join("") + '\n' + displayLast.join("");
+        wordSpaces.innerText = displaySpaces;
+    },
+    //updateWordDisplay method puts letters where they belong after each letter guess
+    updateWordDisplay: function (letter) {
+        displaySpaces = wordSpaces.innerText;
+        for(let i = 0; i < firstName.length; i++){
+            if (firstName[i] === letter) {
+                displayFirst[i] = letter;
+            }
+        }
+        for(let i = 0; i < lastName.length; i++){
+            if(lastName[i] === letter){
+                displayLast[i] = letter;
+            }
+        }
+        displaySpaces = displayFirst.join("") + '\n' + displayLast.join("");
+        wordSpaces.innerText = displaySpaces;       
+    },
+    //guess method makes the letter pressed uppercase and checks to see if it is in the 
+    //word and then determines if you have completed the word and won
+    guess: function (event) {
+        let letter = event.key.toUpperCase();
+        this.lettersArr(letter);
+        if(!displaySpaces.includes('_')){
+            this.win();
+        } else if (this.guessesRemaining < 1) {
+            this.lose();
         }
     },
-    guess: function(event){
-        console.log("guess");
-        this.guessesRemaining = this.guessesRemaining - 1;
-        console.log("keyboard event: " + event.key);
-        let letter = event.key;
-        this.lettersArr.push(letter);
-        console.log(this.lettersArr);
-        lettersGuessed.innerText += letter;
-        console.log("guesses remaining: " + this.guessesRemaining);
-        guessesRemainingId.innerText = this.guessesRemaining;
-        if(this.guessesRemaining < 1){
-            lettersGuessed.innerText = '';
-            console.log("Sorry, you lost!");
-            this.guessesRemaining = 12;
-            num++;
-            console.log(num);
-            this.currentWord();
-            this.wordDisplay(this.currentWord());
-            this.lettersArr = [];
-        
+    //lettersArr method updates the letters guessed area on the screen to show
+    //player which letters they have guessed
+    //also prevents guessing a letter more than once
+    lettersArr: function (letter) {
+        console.log("lettersArr");
+        if (! lettersGuessed.innerText.includes(letter)) {
+            lettersGuessed.innerText += letter;
+            this.guessesRemaining = this.guessesRemaining - 1;
+            guessesRemainingId.innerText = this.guessesRemaining;
+            this.updateWordDisplay(letter);
         }
-        console.log(this.lettersArr);
+
     },
-    win: function(){
-        console.log("win");
+    //win method alerts player they won and resets the board with the next word
+    win: function () {
+        alert(`Winner! ${firstName} ${lastName} was correct!`);
         this.wins += 1;
+        winsTotal.innerText = this.wins;
+        num++;
+        displayFirst = [];
+        displayLast = [];
+        this.currentWord();
         this.guessesRemaining = 12;
+        guessesRemainingId.innerText = this.guessesRemaining;
         this.lettersGuessed = [];
+        lettersGuessed.innerText = '';
+        this.wordDisplay(this.currentWord());
+        if (num === 0){
+            imgDiv.innerHTML = `<img class= "img-fluid" src= "assets/images/${
+                this.words[this.words.length - 1]
+            }.jpg" alt= "image of ${
+                this.words[this.words.length - 1]
+            }">`;
+            songTitle.innerText = this.songs[this.words.length - 1];
+        } else {
+            imgDiv.innerHTML = `<img class= "img-fluid" src= "assets/images/${
+                this.words[num - 1]
+            }.jpg" alt= "image of ${
+                this.words[num - 1]
+            }">`;
+            songTitle.innerText = this.songs[num - 1];
+        } 
+        audio.src = `assets/songs/${songTitle.innerText}.mp3`;
+        
+    },
+    //lose method alerts player if they lose and resets the board for the current word
+    lose: function(){
+        alert(`Sorry, You lost that one. ${firstName} ${lastName} was the correct answer.`);
+        num++;
+        displayFirst = [];
+        displayLast = [];
+        this.currentWord();
+        this.guessesRemaining = 12;
+        guessesRemainingId.innerText = this.guessesRemaining;
+        this.lettersGuessed = [];
+        lettersGuessed.innerText = '';
+        this.wordDisplay(this.currentWord());
+        if(num === 0) {
+            imgDiv.innerHTML = `<img class= "img-fluid" src= "assets/images/${
+                this.words[this.words.length - 1]
+            }.jpg" alt= "image of ${
+                this.words[this.words.length - 1]
+            }">`;
+        } else {
+            imgDiv.innerHTML = `<img class= "img-fluid" src= "assets/images/${
+                this.words[num - 1]
+            }.jpg" alt= "image of ${
+                this.words[num - 1]
+            }">`;
+        } 
     }
-  
+
 }
-
+//listening for key down presses and going into the start method
 document.addEventListener("keydown", hangman.start(), once);
-
-
